1005e9c2fd570eafef3c3a58224eef35b68be418
plinh75 <nguyetnemm2001@gmail.com>
2022-05-20 10:42:56 +0700

    form add reset

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -12,8 +13,53 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(formData: {name: string, age:string}) {
-    console.log(formData);
+  inputValues= {
+    id:0,
+    name: '',
+    age: 0,
+    email: ''
+  };
+
+  users = [
+    {
+      id:1,
+      name: 'linhltpph',
+      age:21,
+      email: 'linhltpph@gmail.com'
+    },
+    {
+      id:15,
+      name: 'linhltpph2',
+      age:22,
+      email: 'linhltpph2@gmail.com',
+    },
+    {
+      id:10,
+      name: 'linhltpph5',
+      age:22,
+      email: 'linhltpph5@gmail.com'
+    }
+  ]
+
+  onSubmit(userForm: NgForm) {
+  //1. tìm id lớn nhất
+    const userIds = this.users
+    .map(user => user.id)
+    .sort((a,b) => a-b)
+    console.log(userIds);
+    const newId = userIds[userIds.length - 1];
     
+    //2. thêm vào mảng
+    this.users.push({
+      ...userForm.value, //lấy ra obj giá trị của form
+      id: newId + 1
+    })
+
+    //3. cập nhật lại giá trị ban đầu
+    userForm.resetForm({
+      name: '',
+      age:0,
+      email: ''
+  })
   }
 }
